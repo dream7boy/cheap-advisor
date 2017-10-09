1dc112a5650235b5cf1890ed73af195ce7346ec4
junggunlee <leejunggun7@gmail.com>
2017-10-09 11:00:02 +0900

    make controller restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+  # get '/', to: 'pages#home'
+  root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
