fc3e1297bea491d824f97ec2d8f976dff265083f
junggunlee <leejunggun7@gmail.com>
2017-10-10 09:16:22 +0900

    add restaurant model

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,25 @@
 class RestaurantsController < ApplicationController
+  # FAKE DB
+  RESTAURANTS = [
+    { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
+    { name: "Sushi Samba", address: "City, London", category: "japanese" }
+  ]
+
+  def index
+    # @restaurants = Restaurant.all
+    @category = params[:category]
+    # raise => debugger
+
+    @restaurants = RESTAURANTS
+  end
+
+  def create
+    raise
+    render plain: "Add to DB restaurant '#{params[:name]}' with address '#{params[:address]}'"
+  end
+
+  def show
+    id = params[:id].to_i
+    @restaurant = RESTAURANTS[id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # get '/', to: 'pages#home'
   root to: 'pages#home'
+  get 'restaurants', to: "restaurants#index"
+  get 'restaurants/:id', to: "restaurants#show"
+  post 'restaurants', to: "restaurants#create"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
